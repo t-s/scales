31974bfdd9a453ce7f2a2278899aff620589e490
Tim Sidie <timsidie@gmail.com>
2023-11-14 13:00:06 -0500

    Following a hello world

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 use bevy::prelude::*;
 
+
+#[derive(Component)]
+struct Name(String);
+#[derive(Component)]
+struct Person;
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+    .add_systems(Startup, add_people)
+    .add_systems(Update, (hello_world, greet_people))
+    .run();
 }
